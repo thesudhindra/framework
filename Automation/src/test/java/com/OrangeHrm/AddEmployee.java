132ec00016c232a8f1f89c9d1edc8a42c4f7585f
sdeshpande <sdeshpande@entrata.com>
2022-05-24 13:58:11 +0530

    commiting Addemployee

--- Automation/src/test/java/com/OrangeHrm/AddEmployee.java
+++ Automation/src/test/java/com/OrangeHrm/AddEmployee.java
@@ -4,6 +4,9 @@ import com.orangeHrm.baseClass;
 import org.testng.annotations.BeforeMethod;
 
 public class AddEmployee extends baseClass {
+    public static void main(String[] args) {
+        
+    }
 
 
 }
